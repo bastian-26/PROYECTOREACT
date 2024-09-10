8d927366fbc45115332b14f71b3a5f845ca324fe
bastian-26 <167238667+bastian-26@users.noreply.github.com>
2024-09-10 12:48:10 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https: /bastian-26.github.io/PROYECTOREACT"
+  base: "https: /bastian-26.github.io/PROYECTOREACT/proyectofinal"
 })
